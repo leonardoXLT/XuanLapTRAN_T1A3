16b81310a1f21b6ba45654290cb03cacd06fb4fc
leonardoXLT <148663990+leonardoXLT@users.noreply.github.com>
2023-12-24 21:12:29 +1100

    add a functions.py file to separate the function codes, add a back up file mainbkup for error handling and testing

--- src/mainbkup.py
+++ src/mainbkup.py
@@ -1,8 +1,21 @@
+from functions import add_mempal, create_mempal, view_edit_mempal, minigame
 import csv
 import random
 import statistics
 import os
 
+
+file_name = "Mempal.csv"
+
+try:
+    mempal_file = open(file_name, "r")
+    mempal_file.close()
+    print("In try block")
+except FileNotFoundError:
+    mempal_file = open(file_name, "w")
+    mempal_file.write("")
+    mempal_file.close()
+    print("In except block")
 
 def add_mempal(existing_loci=None):
     loci = existing_loci if existing_loci else []
@@ -142,3 +155,30 @@ def minigame(file_name):
     with open(file_name, "w", newline="") as f:
         writer = csv.writer(f)
         writer.writerows(memory_palaces)
+
+def menu():
+    print("Welcome to MemPal - your Memory Manager!")
+    print("\nMenu:")
+    print("1. Create New Memory Palace")
+    print("2. View/Edit Memory Palace")
+    print("3. Play the mini game")
+    print("4. Exit")
+    choice = input("Choose an option: ")
+    return choice
+
+users_choice = ""
+
+while users_choice != "4":
+    users_choice = menu()
+    if (users_choice == "1"):
+        create_mempal(file_name)
+    elif (users_choice == "2"):
+        view_edit_mempal(file_name)
+    elif (users_choice == "3"):
+        minigame(file_name)
+    elif (users_choice == "4"):
+        break
+    else:
+        print("Invalid Input")
+
+print("THANK YOU FOR USING MEMPAL!!")
